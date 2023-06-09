16a40919c3807118390e93f81803b8e1353c21ad
Huang <aegis5201@gmail.com>
2023-06-09 16:54:14 +0800

    Feat: firebase deployed

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmfmXWtqbUAvWVsnKhf0tVDC8QKNQSX1I",
+    authDomain: "cssgenerator-d3672.firebaseapp.com",
+    projectId: "cssgenerator-d3672",
+    storageBucket: "cssgenerator-d3672.appspot.com",
+    messagingSenderId: "100884624546",
+    appId: "1:100884624546:web:36faa7ade0cdd1be6193fb",
+    measurementId: "G-0ERY48X04X",
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
